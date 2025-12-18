0bf2e0d4e98c283fa0fb97da8f03d4e84fbae582
kylejones200 <kyletjones@gmail.com>
2025-12-18 16:30:08 -0600

    Bump version to 0.4.0 in ts2net/__init__.py

--- ts2net/__init__.py
+++ ts2net/__init__.py
@@ -7,7 +7,7 @@ Clean API inspired by ts2vg, extended for multiple network methods.
 from .core.graph import Graph
 from .api import HVG, NVG, RecurrenceNetwork, TransitionNetwork, build_network
 
-__version__ = "0.2.0"
+__version__ = "0.4.0"
 
 __all__ = [
     'Graph',
